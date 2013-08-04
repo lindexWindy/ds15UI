666c4b58389be1e2e3aca78b82b8be2e45647a70
lindexWindy <1179091771@qq.com>
2013-08-04 14:37:08 +0800

    new file: map editor
    地图编辑器的架构

---
+++ Ui_2DReplay/Ui_MapEditor.py
@@ -0,0 +1,18 @@
+#
+#ver 0 edited at 
+#map editor
+#
+
+from Ui_ReplayScene import *
+
+
+class Ui_MapEditor(Ui_ReplayView):
+    #def __init__(self):
+
+    #def Initialize(self):
+
+    #def ChangeTerrain(self, terrain):
+
+    #def AddUnits(self, side, position):
+
+    #def Clear = Initialize
