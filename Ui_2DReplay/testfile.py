dc713ce08af62ad2c3305943787a06e2a3512542
lindexWindy <1179091771@qq.com>
2013-08-05 18:04:00 +0800

    debug time
    测试代码。
    这只是一个开始……

--- Ui_2DReplay/testfile.py
+++ Ui_2DReplay/testfile.py
@@ -9,5 +9,7 @@ if __name__=="__main__":
     scene = QtGui.QGraphicsScene()
     view = Ui_2DReplayWidget(scene)
     view.setBackgroundBrush(QtGui.QColor(0, 0, 0))
+    view.Initialize(iniInfo, begInfo[0])
+    view.UpdateEndData(cmd[0], endInfo[0])#bug
     view.show()
     sys.exit(app.exec_())
